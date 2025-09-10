8740d69afd8554836eed1cedac84d47e5a453ccd
rajulajeevmani-hub <rajulajeevamani@gmail.com>
2025-09-10 20:38:34 +0530

    files added

---
+++ script.js
@@ -0,0 +1,5 @@
+const navToggle = document.getElementById('navToggle');
+        const navLinks = document.getElementById('navLinks');
+        navToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('show');
+        });
